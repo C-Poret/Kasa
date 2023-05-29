3797a937f58b3d0bd20b531ed8fb8ae38a6117e4
PORET Corentin <corentin.poret@outlook.fr>
2023-05-29 18:19:53 +0200

    Page d'erreur

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 
 const Error = () => {
     return (
-        <div>
-            
+        <div className='containerError'>
+            <p className='numberError'>404</p>
+            <p className='textError'>Oups! La page que vous demandez n'existe pas.</p>
+            <NavLink className='linkError' to="/">Retournez sur la page d'accueil</NavLink>
         </div>
     );
 };
