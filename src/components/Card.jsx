4b8f5a6a9afd3dcbf0ed583d7087ade22b3e62ec
PORET Corentin <corentin.poret@outlook.fr>
2023-06-05 15:17:24 +0200

    Modification style card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Card = ({cover, title}) => {
     return (
-        <article className='card'>
-            <img src={cover} alt={title} />
+        <article className='card' style={{backgroundImage: "linear-gradient(rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.7))," + `url(${cover})`, backgroundSize: "cover"}}>
             <h2>{title}</h2>
         </article>
     );
